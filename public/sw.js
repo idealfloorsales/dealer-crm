4bb95f7a2851f6e3dfac0fa08fb7d9c2d72e462e
idealfloorsales <idealfloor.sales@gmail.com>
2025-11-21 15:58:09 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'dealer-crm-cache-v45';
 
 const urlsToCache = [
     '/',
-    '/index.html?v=47', // (И здесь тоже меняем на 45)
-    '/style.css?v=47',
-    '/script.js?v=47',
+    '/index.html?v=51', // (И здесь тоже меняем на 45)
+    '/style.css?v=51',
+    '/script.js?v=51',
     '/dealer.html?v=45',
     '/dealer.js?v=45',
     '/map.html?v=45',
